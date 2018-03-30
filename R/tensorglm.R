7b31de46a526e046db5e8ffd5e11d1183dc6bbc0
jtrecenti <julio.trecenti@gmail.com>
2018-03-30 04:32:21 -0300

    adicionando glmk (keras)

--- R/tensorglm.R
+++ R/tensorglm.R
@@ -14,20 +14,27 @@
 #' @export
 glmtf <- function(formula, family = gaussian, data, n = 1000, lr = 0.5,
                   clip = 1.0, scale = TRUE, verbose = FALSE) {
+
   tf <- tensorflow::tf
+
   X <- matrix(model.matrix(formula, data = data)[, -1], nrow(data))
-  maxes <- 1
+  maxes <- 1.0
   if (scale) {
     X <- X / maxes
     maxes <- apply(X, 2, max)
     X <- t(t(X) / maxes)
   }
+
+
   X <- tf$to_float(X)
   y <- tf$to_float(matrix(eval(formula[[2]], data), nrow(X)))
+
+
   #-----------------------------------------------------------------------------
   W <- tf$Variable(tf$random_uniform(tensorflow::shape(ncol(X), 1), -1.0, 1.0))
   b <- tf$Variable(tf$zeros(tensorflow::shape(1L)))
   eta <- tf$matmul(X, W) + b
+
   #-----------------------------------------------------------------------------
   if (is.character(family))
     family <- get(family, mode = "function", envir = parent.frame())
@@ -40,35 +47,95 @@ glmtf <- function(formula, family = gaussian, data, n = 1000, lr = 0.5,
   if (family$link == 'log') family$linkinv <- exp
   mu <- family$linkinv(eta)
   eps <- 1e-08
+
   loss <- switch (
     family$family,
     'gaussian' = tf$reduce_mean((y - mu) ^ 2),
     'poisson' = tf$reduce_mean((y * (log((y + eps) / mu) - 1) + mu) * 2),
-    'Gamma' = tf$reduce_mean((y / mu - log(y / mu) - 1) * 2)
+    'Gamma' = tf$reduce_mean((y / mu - log(y / mu) - 1) * 2),
+    "binomial" = tf$reduce_mean()
   )
+
   #-----------------------------------------------------------------------------
   optimizer <- tf$train$GradientDescentOptimizer(lr)
   gvs <- optimizer$compute_gradients(loss)
+  # clipping
   invisible(
-    purrr::map_if(gvs, ~!is.null(.x[[1]]),
-                  ~tf$clip_by_value(.x[[1]], -1.0, 1.0))
+    purrr::map_if(gvs, ~!is.null(.x[[1]]), ~tf$clip_by_value(.x[[1]], -1.0, 1.0))
   )
   train <- optimizer$apply_gradients(gvs)
-  # train <- optimizer$minimize(loss)
   sess <- tf$Session()
   sess$run(tf$global_variables_initializer())
   #-----------------------------------------------------------------------------
   for (step in seq_len(n)) {
     sess$run(train)
     if (verbose && step %% 100 == 0) {
-      WW <- paste(as.character(round(unlist(sess$run(W) / maxes), 4)),
-                  collapse = ', ')
+      WW <- paste(as.character(round(unlist(sess$run(W) / maxes), 4)), collapse = ', ')
       s <- 'Iter: %d, b=%s, W=(%s)\n'
       cat(sprintf(s, step, round(sess$run(b), 4), WW))
     }
   }
   list(b = sess$run(b), W = sess$run(W) / maxes)
 }
+
+# -------------------------------------------------------------------
+glmk <- function(formula, family = gaussian, data, epochs = 1000, lr = 0.01,
+                 batch_size = nrow(data) / 10,
+                 scale = TRUE, verbose = FALSE) {
+
+  X <- matrix(model.matrix(formula, data = data), nrow(data))
+  maxes <- 1.0
+  if (scale) {
+    X <- X / maxes
+    maxes <- apply(X, 2, max)
+    X <- t(t(X) / maxes)
+  }
+  y <- matrix(eval(formula[[2]], data), nrow(X))
+
+  #-----------------------------------------------------------------------------
+  if (is.character(family))
+    family <- get(family, mode = "function", envir = parent.frame())
+  if (is.function(family))
+    family <- family()
+  if (is.null(family$family)) {
+    print(family)
+    stop("'family' not recognized")
+  }
+  if (family$link == 'log') family$linkinv <- exp
+
+  linkinv <- switch (
+    family$link,
+    "identity" = keras::k_identity,
+    'log' = keras::k_exp,
+    'inverse' = function(x) 1/x,
+    'logit' = keras::k_sigmoid,
+  )
+  eps <- 1e-08
+
+  loss <- switch (
+    family$family,
+    'gaussian' = function(y, mu) keras::k_mean((y - mu) ^ 2),
+    'poisson' = function(y, mu) keras::k_mean((y * (keras::k_log((y + eps) / mu) - 1) + mu) * 2),
+    'Gamma' = function(y, mu) keras::k_mean((y / mu - keras::k_log(y / mu) - 1) * 2),
+    "binomial" = function(y, mu) keras::k_mean(y * keras::k_log(mu) + (1 - y) * keras::k_log(1 - mu))
+  )
+
+  X_ <- keras::layer_input(ncol(X))
+  mu_ <- keras::layer_dense(X_, units = 1, use_bias = FALSE, name = "eta") %>%
+    keras::layer_activation(linkinv)
+
+  model <- keras::keras_model(X_, mu_)
+  keras::compile(model, optimizer = keras::optimizer_sgd(lr), loss = loss)
+  keras::fit(model, X, y, epochs = epochs, batch_size = batch_size)
+
+  keras::get_layer(model, "eta") %>%
+    keras::get_weights() %>%
+    dplyr::first() %>%
+    as.vector() %>%
+    magrittr::divide_by(maxes)
+
+}
+
 
 ## modelo GLM
 # coef(m)
